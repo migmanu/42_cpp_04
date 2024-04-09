d6b4d1db922f4141678e00cfced27f6b5d7b593c
Jose Manuel Migoya Bussolotti <jmanuelmigoya@gmai.com>
2024-04-09 14:34:01 +0200

    fix: improve prints for testing

--- ex03/Character.cpp
+++ ex03/Character.cpp
@@ -6,77 +6,83 @@
 /*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 00:26:27 by jmigoya-          #+#    #+#             */
-/*   Updated: 2024/04/09 14:12:48 by jmigoya-         ###   ########.fr       */
+/*   Updated: 2024/04/09 14:32:05 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Character.hpp"
 
-Character::Character( std::string const & name ) : _name(name)
+Character::Character(std::string const &name) : _name(name)
 {
-    for (int i = 0; i < 4; i++) {
-        this->_inventory[i] = NULL;
-    }
-    std::cout << "Character " << this->_name << " created" << std::endl;
+	for (int i = 0; i < 4; i++)
+	{
+		this->_inventory[i] = NULL;
+	}
+	std::cout << "Character " << this->_name << " created" << std::endl;
 }
 
-Character::Character( Character const & src ) : _name(src._name)
+Character::Character(Character const &src) : _name(src._name)
 {
-    *this = src;
-    std::cout << "Character " << this->_name << " created" << std::endl;
+	*this = src;
+	std::cout << "Character " << this->_name << " created" << std::endl;
 }
 
-Character& Character::operator=( Character const & rhs )
+Character &Character::operator=(Character const &rhs)
 {
-    if (this != &rhs)
-    {
-        this->_name = rhs._name;
-        for (int i = 0; i < 4; i++)
-            this->_inventory[i] = rhs._inventory[i];
-    }
-    return *this;
+	if (this != &rhs)
+	{
+		this->_name = rhs._name;
+		for (int i = 0; i < 4; i++)
+			this->_inventory[i] = rhs._inventory[i];
+	}
+	return *this;
 }
 
 Character::~Character()
 {
-    std::cout << "Character " << this->_name << " destroyed" << std::endl;
-    for (int i = 0; i < 4; i++)
-        if (this->_inventory[i])
-            delete this->_inventory[i];
+	std::cout << "Character " << this->_name << " destroyed" << std::endl;
+	for (int i = 0; i < 4; i++)
+		if (this->_inventory[i])
+			delete this->_inventory[i];
 }
 
-void    Character::equip( AMateria* m ) {
-    for (int i = 0; i < 4; i++)
-        if (this->_inventory[i] == NULL)
-        {
-            this->_inventory[i] = m;
-            std::cout << "Character " << this->_name << " equipped with " << m->getType() << std::endl;
-            return;
-        }
-    std::cout << "Character " << this->_name << " can't equip " << m->getType() << std::endl;
+void Character::equip(AMateria *m)
+{
+	for (int i = 0; i < 4; i++)
+		if (this->_inventory[i] == NULL)
+		{
+			this->_inventory[i] = m;
+			std::cout << "Character " << this->_name << " equipped with " << m->getType() << std::endl;
+			return;
+		}
+	std::cout << "Character " << this->_name << " can't equip " << m->getType() << std::endl;
+	delete m;
 }
 
-void    Character::unequip( int idx ) {
-    if (this->_inventory[idx])
-    {
-        delete this->_inventory[idx];
-        this->_inventory[idx] = NULL;
-        std::cout << "Character " << this->_name << " unequipped" << std::endl;
-    }
-    else
-        std::cout << "Character " << this->_name << " can't unequip" << std::endl;
+void Character::unequip(int i)
+{
+	if (this->_inventory[i])
+	{
+		std::cout << "Character " << this->_name << " dropped " << this->_inventory[i]->getType() << std::endl;
+		delete this->_inventory[i];
+		this->_inventory[i] = NULL;
+	}
+	else
+		std::cout << "Character can't drop " << this->_name << std::endl;
 }
 
-void    Character::use( int idx, ICharacter& target ) {
-    if (this->_inventory[idx])
-    {
-        this->_inventory[idx]->use(target);
-        std::cout << "Character " << this->_name << " uses " << this->_inventory[idx]->getType() << std::endl;
-    }
-    else
-        std::cout << "Character " << this->_name << " can't use" << std::endl;
+void Character::use(int i, ICharacter &target)
+{
+	if (this->_inventory[i])
+	{
+		this->_inventory[i]->use(target);
+		std::cout << "Character " << this->_name << " uses " << this->_inventory[i]->getType() << std::endl;
+	}
+	else
+		std::cout << "Character " << this->_name << " can't use" << std::endl;
 }
 
-std::string const& Character::getName() const {
-    return this->_name;
+std::string const &Character::getName() const
+{
+	return this->_name;
 }
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,29 +6,48 @@
 /*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 00:15:08 by jmigoya-          #+#    #+#             */
-/*   Updated: 2024/04/04 00:30:58 by jmigoya-         ###   ########.fr       */
+/*   Updated: 2024/04/09 14:32:46 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Character.hpp"
 #include "Cure.hpp"
 #include "Ice.hpp"
-#include "Character.hpp"
 #include "MateriaSource.hpp"
 
 int main(void)
 {
+	std::string divider = "-------------------";
+
+	std::cout << std::endl << divider << "START PROGRAM" << divider << std::endl << std::endl;
+	std::cout << std::endl << divider << "Instanciate objects" << divider << std::endl;
 	IMateriaSource *src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
 	ICharacter *me = new Character("me");
 	AMateria *tmp;
+	std::cout << std::endl << divider << "Add materias to Character" << divider << std::endl;
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
+	std::cout << std::endl << divider << "Surpass max Materias" << divider << std::endl;
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+	std::cout << std::endl << divider << "Unequip Materia and add another" << divider << std::endl;
+	me ->unequip(2);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+	std::cout << std::endl << divider << "Attack and cure" << divider << std::endl;
 	ICharacter *bob = new Character("bob");
 	me->use(0, *bob);
-	me->use(1, *bob);
+	me->use(2, *bob);
+	me->use(3, *bob);
+	std::cout << std::endl << divider << "Delete all" << divider << std::endl;
 	delete bob;
 	delete me;
 	delete src;
